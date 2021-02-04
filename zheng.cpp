522d6539e9a1803183b9a38a642e23cdf3c9cede
Delfad0r <filippo.gianni.baroni@gmail.com>
2021-02-04 18:09:14 +0100

    gio 4 feb 2021, 18:09:14, CET

--- zheng.cpp
+++ zheng.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <mutex>
 #include <thread>
+#include <random>
 #include <string>
 #include <vector>
 
@@ -114,77 +115,6 @@ public:
     }
 };
 
-template<typename I, typename T>
-auto compute_r_and_s(T d) {
-    auto p_table = partitions_table<T>(d);
-    auto conj = conjugacy_classes<I, T>(p_table);
-    auto mult = multiplication_table(p_table);
-    auto ch = character_tables<I>(p_table);
-    std::vector<I> factorial(d + 1), factorial_inv(d + 1);
-    factorial[0] = factorial_inv[0] = 1;
-    for(uint32_t j = 1; j <= uint32_t(d); ++j) {
-        factorial[j] = factorial[j - 1] * I(j);
-        factorial_inv[j] = factorial_inv[j - 1] / I(j);
-    }
-    std::vector<std::vector<std::vector<I>>> scoeff(d + 1);
-    for(T i = 0; i <= d; ++i) {
-        scoeff[i].resize(ch[i].size());
-        for(uint32_t j = 0; j < ch[i].size(); ++j) {
-            scoeff[i][j].resize(ch[i][j].size());
-            for(uint32_t k = 0; k < ch[i][j].size(); ++k) {
-                scoeff[i][j][k] = ch[i][j][k] * conj[i][k] / ch[i][j][0];
-            }
-        }
-    }
-    
-    timer::start("r and s");
-    std::vector<std::tuple<I, std::vector<std::tuple<I, uint32_t>>>> ans;
-    iterate_on_secondary_partitions(p_table, d, [&](auto omega) {
-        uint32_t k = omega.size();
-        // Compute r(omega)
-        I r = (k % 2) ? (1) : (-1);
-        r *= factorial[k - 1];
-        btree::btree_map<std::tuple<T, uint32_t>, uint32_t> multiplicities;
-        for(const auto& [i, j] : omega) {
-            ++multiplicities[{i, j}];
-        }
-        for(const auto& [i, j] : multiplicities) {
-            r *= factorial_inv[j];
-        }
-        for(const auto& [i, j] : omega) {
-            I x = ch[i][j][0] * factorial_inv[i];
-            r *= x * x;
-        }
-        // Compute s(omega)
-        multivariate_polynomial<I, T> s = { 0, {{1, 0}} };
-        for(const auto& [i, j] : omega) {
-            decltype(s) p = { i, {} };
-            std::get<1>(p).reserve(p_table[i].size());
-            for(uint32_t nu = 0; nu < p_table[i].size(); ++nu) {
-                std::get<1>(p).emplace_back(scoeff[i][j][nu], nu);
-            }
-            s = mult_two_polynomials(mult, s, p); // FASTER
-        }
-        /*std::vector<multivariate_polynomial<I, T>> polys;
-        for(const auto& [i, j] : omega) {
-            multivariate_polynomial<I, T> p = { i, {} };
-            std::get<1>(p).reserve(p_table[i].size());
-            for(uint32_t nu = 0; nu < p_table[i].size(); ++nu) {
-                std::get<1>(p).emplace_back(scoeff[i][j][nu], nu);
-            }
-            polys.emplace_back(std::move(p));
-        }
-        auto s = mult_many_polynomials(mult, std::move(polys));*/
-        std::get<1>(s).erase(std::get<1>(s).begin());
-        std::get<1>(s).shrink_to_fit();
-        // Return
-        ans.emplace_back(r, std::get<1>(s));
-    });
-    ans.shrink_to_fit();
-    timer::end("r and s");
-    return ans;
-}
-
 
 template<typename I, typename T, uint32_t n>
 auto exceptional_partitions(uint32_t d) {
@@ -202,9 +132,19 @@ auto exceptional_partitions(uint32_t d) {
     std::mutex mutex;
     std::vector<std::array<uint32_t, n>> exceptional;
     const uint32_t CHUNK_SIZE = std::min(uint32_t(P.size() - 1) / THREADS + 1, uint32_t(RAM / (THREADS * sizeof(I) * pow(P.size(), n - 1))));
-    std::cerr << CHUNK_SIZE << std::endl;
-    std::atomic<uint32_t> first_p = 1;
-
+    uint32_t first_p = 1, completed = 0;
+    
+    std::vector<uint32_t> perm(P.size()), inv_perm(P.size());
+    std::iota(perm.begin(), perm.end(), 0);
+    std::shuffle(perm.begin() + 1, perm.end(), std::mt19937(std::random_device{}()));
+    for(uint32_t i = 0; i < perm.size(); ++i) {
+        inv_perm[perm[i]] = i;
+    }
+    
+    std::cerr << "Partitions: " << P.size() << std::endl;
+    std::cerr << "Chunk size: " << CHUNK_SIZE << std::endl;
+    
+    timer::start("compute");
     for(int t = 0; t < THREADS; ++t) {
         threads.emplace_back([&, t]() {
             while(true) {
@@ -216,18 +156,24 @@ auto exceptional_partitions(uint32_t d) {
                     if(myp >= P.size()) {
                         break;
                     }
-                    std::cerr << myp << " / " << P.size() << "    in " << timer::elapsed("compute") << "s" << std::endl;
                 }
                 ndvector<n, I> myres(std::tuple_cat(std::tuple(CHUNK_SIZE), make_ntuple<n - 1, size_t>(P.size())));
+                std::vector<bool> visited(CHUNK_SIZE);
                 RS.iterate([&](I r, const auto& s) {
                     std::array<uint32_t, n> p;
                     uint32_t sum_len = 0;
                     for(uint32_t i0 = 0; i0 < s.size(); ++i0) {
-                        if(std::get<1>(s[i0]) < myp or std::get<1>(s[i0]) >= myp + CHUNK_SIZE) {
+                        if(perm[std::get<1>(s[i0])] < myp or perm[std::get<1>(s[i0])] >= myp + CHUNK_SIZE) {
                             continue;
                         }
-                        p[0] = std::get<1>(s[i0]) - myp;
-                        sum_len = PS[p[0] + myp];
+                        p[0] = perm[std::get<1>(s[i0])] - myp;
+                        if(not visited[p[0]]) {
+                            visited[p[0]] = true;
+                            std::scoped_lock lock(mutex);
+                            ++completed;
+                            std::cerr << "[" << ((t < 10) ? (" ") : ("")) << t << "]   "  << completed << " / " << P.size() << "    in " << timer::elapsed("compute") << "s" << std::endl;
+                        }
+                        sum_len = PS[std::get<1>(s[i0])];
                         std::y_combinator([&](auto rec, uint32_t k, uint32_t j, I c) -> void {
                             for(uint32_t i = j; i < s.size(); ++i) {
                                 p[k] = std::get<1>(s[i]);
@@ -250,23 +196,23 @@ auto exceptional_partitions(uint32_t d) {
                     uint32_t sum_len = 0;
                     for(uint32_t i0 = myp; i0 < myp + CHUNK_SIZE and i0 < P.size(); ++i0) {
                         p[0] = i0 - myp;
-                        sum_len = PS[i0];
+                        sum_len = PS[inv_perm[i0]];
                         std::y_combinator([&](auto rec, uint32_t k, uint32_t j) -> void {
                             for(uint32_t i = j; i < P.size(); ++i) {
                                 p[k] = i;
                                 sum_len += PS[i];
                                 if(k + 1 == n) {
                                     if(hurwitz(sum_len) and myres[array_to_tuple(p)] == 0) {
-                                        p[0] += myp;
+                                        p[0] = inv_perm[i0];
                                         myexc.push_back(p);
-                                        p[0] -= myp;
+                                        p[0] = i0 - myp;
                                     }
                                 } else {
                                     rec(k + 1, i);
                                 }
                                 sum_len -= PS[p[k]];
                             }
-                        })(1, i0);
+                        })(1, inv_perm[i0]);
                     }
                     std::scoped_lock lock(mutex);
                     exceptional.insert(exceptional.end(), myexc.begin(), myexc.end());
